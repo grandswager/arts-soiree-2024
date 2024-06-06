2af4b2de92cd1cd799f0e53b095538a26d0613ff
grandswager <96254408+grandswager@users.noreply.github.com>
2024-06-06 18:04:08 +0000

    Twelfth commit
    French is broken. AGAIN.

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ let elements = document.getElementsByTagName("*");
 
 /* Nice fade-in for article */
 for (let element = 0; element < elements.length; element++) {
-  elements[element].style.animation = "transition-in .5s ease-in-out forwards";
-  elements[element].style.animationDelay = ".5s";
+  if (elements[element].tagName != "BODY") {
+    elements[element].style.animation = "transition-in .5s ease-in-out forwards";
+    elements[element].style.animationDelay = ".5s";
+    console.log(elements[element].tagName)
+  } else {
+    elements[element].style.opacity = "1";
+    console.log("body")
+  }
 }
